7aa2fb5fa8f9208e390f7224d800e26d9287674f
Atul Verma <atulverma@192.168.0.104>
2023-12-31 18:47:14 +0530

    Added document loaders

---
+++ NotUI/utils/doc_utils.py
@@ -0,0 +1,72 @@
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import UnstructuredPDFLoader
+from langchain.document_loaders import OnlinePDFLoader
+from langchain.document_loaders import UnstructuredURLLoader
+from typing import Union, List
+import logging
+
+from utils.logging_utils import custom_logging
+custom_logging()
+
+def load_txt(file_path: str, encoding: str = None)-> List:
+    """Load text file from given path.
+
+    Args:
+        file_path (str): Path to the text file
+        encoding (str, optional): Encoding to be used for the file. Defaults to None.
+
+    Returns:
+        List: List of Langchain Document objects
+    """
+    
+    documents = TextLoader(file_path, encoding).load()
+    logging.info(f'Loaded {len(documents)} documents from [TEXT FILE]')
+    return documents
+
+
+def load_pdf(file_path: str)-> List:
+    """Load PDF file from given path.
+
+    Args:
+        file_path (str): Path to the PDF file
+
+    Returns:
+        List: List of Langchain Document objects
+    """
+    
+    documents = UnstructuredPDFLoader(file_path, mode='single', strategy='fast').load()
+    logging.info(f'Loaded {len(documents)} documents from [PDF FILE]')
+    return documents
+    
+    
+def load_online_pdf(path: str)-> List:
+    """Load PDF file from given URL.
+
+    Args:
+        path (str): URL to the PDF file
+
+    Returns:
+        List: List of Langchain Document objects
+    """
+    
+    documents = OnlinePDFLoader(path).load()
+    logging.info(f'Loaded {len(documents)} documents from [ONLINE PDF]')
+    return documents
+
+def load_url(url: str)-> List:
+    """Load URL from given path.
+
+    Args:
+        url (str): List of URLs
+
+    Returns:
+        List: List of Langchain Document objects
+    """
+    
+    documents = UnstructuredURLLoader(url, mode='single', strategy='fast').load()
+    logging.info(f'Loaded {len(documents)} documents from [URL]')
+    return documents
+    
+def load_and_split_doc():
+    pass
+    
