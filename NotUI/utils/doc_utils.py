93ef8a81840653f491a01bb1b3861bfa6b58871e
Atul Verma <atulverma@192.168.0.104>
2023-12-31 20:54:13 +0530

    Added Splitter config

--- NotUI/utils/doc_utils.py
+++ NotUI/utils/doc_utils.py
@@ -2,10 +2,11 @@ from langchain.document_loaders import TextLoader
 from langchain.document_loaders import UnstructuredPDFLoader
 from langchain.document_loaders import OnlinePDFLoader
 from langchain.document_loaders import UnstructuredURLLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from typing import Union, List
 import logging
 
-from utils.logging_utils import custom_logging
+from logging_utils import custom_logging
 custom_logging()
 
 def load_txt(file_path: str, encoding: str = None)-> List:
@@ -53,20 +54,29 @@ def load_online_pdf(path: str)-> List:
     logging.info(f'Loaded {len(documents)} documents from [ONLINE PDF]')
     return documents
 
-def load_url(url: str)-> List:
+def load_url(urls: List[str])-> List:
     """Load URL from given path.
 
     Args:
-        url (str): List of URLs
+        urls List[str]: List of URLs
 
     Returns:
         List: List of Langchain Document objects
     """
     
-    documents = UnstructuredURLLoader(url, mode='single', strategy='fast').load()
+    documents = UnstructuredURLLoader(urls, mode='single', strategy='fast').load()
     logging.info(f'Loaded {len(documents)} documents from [URL]')
     return documents
     
-def load_and_split_doc():
-    pass
+def load_and_split_doc(paths: List)-> List:
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=100,
+        length_function=len
+        )
+    docs = []
     
+    #TODO - Fill docs dynamically
+    
+    splitter.split_documents(docs)
+    pass
