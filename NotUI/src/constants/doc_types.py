35b163f3f122a2b890a439c7d39cd43fe67a703c
Denver04 <=>
2024-02-02 22:36:51 +0530

    loader and splitter added

--- NotUI/src/constants/doc_types.py
+++ NotUI/src/constants/doc_types.py
@@ -3,6 +3,6 @@ from enum import Enum
 class DocType(Enum):
     PDF='pdf'
     ONLINE_PDF='omline_pdf'
-    TXT='txt'
-    WEB_URL='web_url'
+    TXT = 'txt'
+    WEB_URL = 'web_url'
     
---
+++ backend/src/constants/enum.ts
@@ -0,0 +1,6 @@
+export enum DOC_TYPES {
+  PDF = "pdf",
+  TXT = "txt",
+  ONLINE_PDF = "omline_pdf",
+  WEB_URL = "web_url",
+}
