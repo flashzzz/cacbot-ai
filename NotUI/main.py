964fd7ba41f61290fecb2852dfdea88c26a6af91
Atul Verma <atulverma@Atul-Verma.local>
2024-02-06 01:37:33 +0530

    Add: Pinecone utils addded

--- NotUI/main.py
+++ NotUI/main.py
@@ -1,1 +1,9 @@
 # some cool async fastapi/sanic code
+# from src.utils.vectorstore_utils import VectorStore
+# from src.utils.doc_utils import load_and_split_doc
+# v = VectorStore('user-id')
+# # docs = load_and_split_doc([('/Users/atulverma/Desktop/cacbot-ai/NotUI/abc.txt','txt')],'user-id')
+# # print(docs)
+# # v.upsert(docs)
+# ret = v.semantic_search('what is blueprint',mmr=False,k=5)
+# print(ret)
