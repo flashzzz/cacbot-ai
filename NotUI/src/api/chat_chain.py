2e35e82e8e83fd39cf9544f821730f6d9150447d
Atul Verma <atulverma@Atul-Verma.local>
2024-02-10 02:06:59 +0530

    Add: Added chat chain

---
+++ NotUI/src/api/chat_chain.py
@@ -0,0 +1,25 @@
+import os
+from langchain_core.documents import Document
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+
+
+def chat_chain(user_query: str, context: str)-> str:
+    llm = ChatOpenAI(api_key=os.environ.get("OPENAI_API_KEY"), model="gpt-3.5-turbo")
+
+    prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
+<context>
+{context}
+</context>
+Question: {input}""")
+
+    document_chain = create_stuff_documents_chain(llm, prompt)
+
+    llm_response = document_chain.invoke({
+    "input": user_query,
+    "context": [Document(page_content=context)]
+})
+    return llm_response
+
+chat_chain()
