f58ba80d77ba7a04b1292dbccc0febd1c0bc82ce
Denver04 <=>
2024-02-19 14:38:05 +0530

    refine folder structure

--- backend/index.ts
+++
@@ -1,4 +0,0 @@
-
-let num: number = 12;
-console.log(num);
-console.log(process.env.PINECONE_API_KEY)
--- backend/src/constants/enum.ts
+++
@@ -1,6 +0,0 @@
-export enum DOC_TYPES {
-  PDF = "pdf",
-  TXT = "txt",
-  ONLINE_PDF = "omline_pdf",
-  WEB_URL = "web_url",
-}
